c46477f263fad0523340f795cdce56259053d935
aheri30 <elianniscortez@gmail.com>
2025-04-22 22:38:22 +0000

    Lista despegable

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,10 +1,14 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
-], (Controller) => {
+], function (Controller) {
     "use strict";
 
     return Controller.extend("company.trial.controller.MainView", {
-        onInit() {
+        onInit: function () {
+            const oJSONModel = new sap.ui.model.json.JSONModel(); //Instancia para el modelo
+            const oView = this.getView(); //Instancia para la vista
+            oJSONModel.loadData("./model/SelectionScreenMenu.json"); //Cargar Data JSon
+            oView.setModel(oJSONModel, "selectionScreen"); //Asignar modelo a la vista
         }
     });
 });
